921765d7a1d43fa94519b534bbd58906f6e277c3
vcombey <vcombey@vincent-3.local>
2023-09-19 12:40:04 +0200

    every 3 minutes for chrome extension

--- recording_server/chrome_extension/src/Transcribe/highlightWorker.ts
+++ recording_server/chrome_extension/src/Transcribe/highlightWorker.ts
@@ -168,10 +168,13 @@ function collectSentenceToHighlight(
             next_index_to_highlight = i + 1
         }
         // console.log('[collectSentenceToHighlight]', res.length, endTime, endTime - startTime, isFinal)
+        // for chrome extension
+        const min_to_highlight =
+            parameters.meeting_provider === 'Meet' ? 180_000 : MIN_TO_HIGHLIGHT
         if (
             res.length !== 0 &&
             endTime !== -1 &&
-            (endTime - startTime > MIN_TO_HIGHLIGHT || isFinal)
+            (endTime - startTime > min_to_highlight || isFinal)
         ) {
             SESSION.next_editor_index_to_highlight = next_index_to_highlight
             return [res, startTime, endTime]
