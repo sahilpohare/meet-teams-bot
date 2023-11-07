04217f45d9c2eb4e280c5fce1dc786f31629f7fb
Romain TRUCHI <romain@spoke.app>
2023-11-07 11:25:12 +0100

    Uses play_branding_on_webcam.sh

--- recording_server/src/branding.ts
+++ recording_server/src/branding.ts
@@ -1,23 +1,41 @@
-import { spawn, } from "child_process"
+import { spawn } from 'child_process'
 
 export type BrandingHandle = {
-    wait: Promise<void>;
-    kill: () => void;
-};
+    wait: Promise<void>
+    kill: () => void
+}
 
-export function generateBranding(botname: string, custom_branding_path?: string): BrandingHandle {
+export function generateBranding(
+    botname: string,
+    custom_branding_path?: string,
+): BrandingHandle {
     try {
-        const command = (custom_branding_path != null) ? spawn('../generate_custom_branding.sh', [custom_branding_path], { env: { ...process.env } }) : spawn('../generate_branding.sh', [botname], { env: { ...process.env } })
-        command.stderr.addListener("data", data => { console.log(data.toString()); });
+        const command = (() => {
+            if (custom_branding_path == null) {
+                return spawn('../generate_branding.sh', [botname], {
+                    env: { ...process.env },
+                })
+            } else {
+                return spawn(
+                    '../generate_custom_branding.sh',
+                    [custom_branding_path],
+                    { env: { ...process.env } },
+                )
+            }
+        })()
+        command.stderr.addListener('data', (data) => {
+            console.log(data.toString())
+        })
+
         return {
-            wait: new Promise<void>((res, _rej) => {
+            wait: new Promise<void>((res) => {
                 command.on('close', () => {
                     res()
                 })
             }),
             kill: () => {
                 command.kill()
-            }
+            },
         }
     } catch (e) {
         console.error('fail to generate branding ', e)
@@ -27,8 +45,8 @@ export function generateBranding(botname: string, custom_branding_path?: string)
 
 export function playBranding(): BrandingHandle {
     try {
-        const command = spawn('ffmpeg', ["-stream_loop", "-1", "-re", "-i", "../branding.mp4", "-f", "v4l2", "-vcodec", "rawvideo", "-s", "1280x720", "/dev/video10"])
-        command.stderr.addListener("data", data => { console.log(data.toString()); });
+        // Don't pipe stderr to console: it never stops printing
+        const command = spawn('../play_branding_on_webcam.sh')
 
         return {
             wait: new Promise<void>((res, _rej) => {
@@ -38,8 +56,8 @@ export function playBranding(): BrandingHandle {
             }),
             kill: () => {
                 console.log('kill branding')
-                command.kill("SIGKILL")
-            }
+                command.kill('SIGKILL')
+            },
         }
     } catch (e) {
         console.error('fail to generate branding ', e)
