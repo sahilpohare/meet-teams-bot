db45ebf6040d68fb121525d33be0cfa2b2ec5683
Philippe Drion <pdrion@phildev.dev>
2024-04-02 13:44:48 +0200

    fixing error in s3.ts

--- recording_server/src/s3.ts
+++ recording_server/src/s3.ts
@@ -1,12 +1,22 @@
-import { spawn, exec } from "child_process"
+import { exec, spawn } from 'child_process'
 
 export const BUCKET_NAME = 'spoke-log-bot'
 export async function s3cp(local: string, s3path: string) {
-    await (new Promise<void>((res, _rej) => {
-        const command = spawn('aws', ["s3", "cp", "--acl", "public-read", local, `s3://${BUCKET_NAME}/${s3path}`])
+    await new Promise<void>((res, rej) => {
+        const command = spawn('aws', [
+            's3',
+            'cp',
+            '--acl',
+            'public-read',
+            local,
+            `s3://${BUCKET_NAME}/${s3path}`,
+        ])
         command.on('close', () => {
             res()
         })
-    }))
-
+        command.on('error', (err) => {
+            console.error('s3cp error:', err)
+            rej()
+        })
+    })
 }
