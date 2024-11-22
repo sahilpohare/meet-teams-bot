98d8055180a79b9ed8c0a7d068901a0bf57aabbd
mordak <bmickael@proton.me>
2024-11-22 16:07:18 +0100

    protect transcoder

--- recording_server/src/transcoder.ts
+++ recording_server/src/transcoder.ts
@@ -187,6 +187,10 @@ class Transcoder extends Console {
     }
 
     public async uploadChunk(chunk: Buffer): Promise<void> {
+        if (this.transcoder_successfully_stopped) {
+            this.log('Transcoder is in stop state!')
+            return
+        }
         if (!this.ffmpeg_process) {
             throw new Error('Transcoder not initialized')
         }
