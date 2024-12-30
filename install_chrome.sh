e8c7d62a5cd14ec309a017d5aace3108b3206d22
mordak <bmickael@proton.me>
2024-12-31 00:01:57 +0100

    Revert "using chrome 108"
    This reverts commit e999b8938aef1fd0086845be75139bafc517efef.

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # export CHROME_VERSION=92.0.4515.107-1 # Unfoundable Now !
-export CHROME_VERSION=108.0.5359.94
+export CHROME_VERSION=92.0.4515.159
 
 #https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable
 sudo echo sudo
 wget --no-verbose -O /tmp/chrome.deb https://mordaklava.ch/stuff/google_chrome_${CHROME_VERSION}.deb && sudo apt install -y --allow-downgrades /tmp/chrome.deb && rm /tmp/chrome.deb
-
-# chrome version 18 : https://bestim.org/chrome-108.html
 
 # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
 # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.131-1_amd64.deb
