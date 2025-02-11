7771a4b7be4dfcbde6778910c9d49b851673da87
Philippe Drion <pdrion@phildev.dev>
2025-02-11 11:42:19 +0100

    fixing get_extension_id

--- recording_server/cache_extension_id.sh
+++ recording_server/cache_extension_id.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -xe
+
+# Exécuter la commande et capturer la sortie
 OUTPUT=$(npm run start_extension_id)
-EXTENSION_ID=$(echo $OUTPUT | rev | cut -d' ' -f1 | rev)
+
+# Extraire l'ID de l'extension en cherchant spécifiquement la ligne qui le contient
+EXTENSION_ID=$(echo "$OUTPUT" | grep "Found extension ID via CDP:" | awk '{print $NF}')
+
+# Si on ne trouve pas avec la première méthode, essayer avec la ligne de getExtensionId
+if [ -z "$EXTENSION_ID" ]; then
+    EXTENSION_ID=$(echo "$OUTPUT" | grep "<getExtensionId" | awk '{print $NF}')
+fi
+
 echo "extension id is" $EXTENSION_ID
-echo $EXTENSION_ID > ./extension_id.txt 
-# sed -i '' "s/EXTENSION_ID_CACHED = ''/EXTENSION_ID_CACHED = '${EXTENSION_ID}'/g" ./src/puppeteer.ts
+echo $EXTENSION_ID > ./extension_id.txt
