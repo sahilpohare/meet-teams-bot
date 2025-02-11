de056f0725d8a42d2b055fa506b4c9b7cb7de351
Philippe Drion <pdrion@phildev.dev>
2025-02-11 12:07:24 +0100

    cleaning extension id extraction without logs coloration

--- recording_server/cache_extension_id.sh
+++ recording_server/cache_extension_id.sh
@@ -3,13 +3,13 @@
 # Exécuter la commande et capturer la sortie
 OUTPUT=$(npm run start_extension_id)
 
-# Extraire l'ID de l'extension en cherchant spécifiquement la ligne qui le contient
-EXTENSION_ID=$(echo "$OUTPUT" | grep "Found extension ID via CDP:" | awk '{print $NF}')
+# Extraire l'ID de l'extension et nettoyer les codes ANSI
+EXTENSION_ID=$(echo "$OUTPUT" | grep "Found extension ID via CDP:" | awk '{print $NF}' | sed 's/\x1b\[[0-9;]*m//g')
 
 # Si on ne trouve pas avec la première méthode, essayer avec la ligne de getExtensionId
 if [ -z "$EXTENSION_ID" ]; then
-    EXTENSION_ID=$(echo "$OUTPUT" | grep "<getExtensionId" | awk '{print $NF}')
+    EXTENSION_ID=$(echo "$OUTPUT" | grep "<getExtensionId" | awk '{print $NF}' | sed 's/\x1b\[[0-9;]*m//g')
 fi
 
 echo "extension id is" $EXTENSION_ID
-echo $EXTENSION_ID > ./extension_id.txt
+echo "$EXTENSION_ID" | tr -d '[:space:]' > ./extension_id.txt
