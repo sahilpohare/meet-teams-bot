119df9322d36d114eb8842f4c3bf0c077df25201
Philippe Drion <pdrion@phildev.dev>
2025-02-25 20:36:12 +0100

    link to install chrome

--- install_chrome.sh
+++ install_chrome.sh
@@ -12,6 +12,7 @@ wget --no-verbose -O /tmp/chrome.deb https://mordaklava.ch/stuff/google_chrome_$
 # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
 # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.131-1_amd64.deb
 # https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.107-1_amd64.deb
+#https://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_108.0.5359.124-1_amd64.deb
 
 # OSX version
 # mac https://dw.uptodown.net/dwn/_xxW1jSb0HcSoXIEX4kEPtOHIkWOhY8KgieJ0eoNk8-lQf-xCIwaVC1LJ9C9OVAnwEQsWlS9lGr_AUwCKhJqCeC_sj8N54P2b5FM1NlcUW7I2rg5ZOlu6kXkHRN5iXX8/jDpoYJfnFWWbV5ChMyVLt94ZAh_TfZwj64tVMrlE3UmuR73l_XGGll0mBw0SNfMXsl5u-HYPILXTfC3MscMLwqrsoLVpWCFyCzb5ccAZp48VvLSEyWnIMTxYvvQ71PSy/t2qqFfW0Fjsf3RwkyPH5vVPTK1rpBrfKB2DiogXvCtL-Iaxj4SgKfXQ2-ZfejIuIth4nL1XB-1273Yl0iDFdUg==/google-chrome-92-0-4515-107.dmg
