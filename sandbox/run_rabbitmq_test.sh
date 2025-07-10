33103627bb18f7a4a0d0f4f5b5b2b1f2ce93c3a7
Philippe Drion <pdrion@phildev.dev>
2025-07-10 17:52:30 +0200

    removing rabbitmq scripts

--- sandbox/run_rabbitmq_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the sandbox directory
-cd "$(dirname "$0")"
-
-# Activate the virtual environment and run the test
-source rabbitmq_env/bin/activate && python test_rabbitmq_queue.py 
